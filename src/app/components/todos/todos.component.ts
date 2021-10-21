1980dba4f853892144ced79ed67dc984d8ba8828
andreiconstantin8 <andreiconstantin8@yahoo.com>
2021-10-21 13:52:29 +0300

    To do application

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos?: Todo[];
+  inputTodo: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+    ]
+  }
+  toggleDone(id: number) {
+    // @ts-ignore
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+  }
+
+  // delete todo
+  deleteTodo(id: number) {
+    this.todos = this.todos?.filter((v, i) => i !== id);
+  }
+
+  //add todo
+  addTodo() {
+    if (this.inputTodo) {
+      // @ts-ignore
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false
+      })
+    }
+    
+    else {
+      alert("not allowed empty todo");
+
+    }
+    this.inputTodo = "";
+  }
+}
